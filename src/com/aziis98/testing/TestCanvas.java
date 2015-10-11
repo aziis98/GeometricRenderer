3e1b87c01bece4549552b31316b46c9b31f2678f
aziis98 <antonio.delucreziis@gmail.com>
2015-10-12 01:16:28 +0200

    Creating the Canvas rendering structure.

--- src/com/aziis98/testing/TestCanvas.java
+++ src/com/aziis98/testing/TestCanvas.java
@@ -1,4 +1,18 @@
 package com.aziis98.testing;
 
+import com.aziis98.vrenderer.api.*;
+
+import javax.imageio.*;
+import java.io.*;
+
 public class TestCanvas {
+
+    public static void main(String[] args) {
+
+        Canvas canvas = new Canvas();
+
+        ImageIO.write( canvas.renderImage( 800, 600 ), "png", new File( "res/TestResult.png" ) );
+
+    }
+
 }
--- src/com/aziis98/vrenderer/api/Canvas.java
+++ src/com/aziis98/vrenderer/api/Canvas.java
@@ -1,7 +1,23 @@
 package com.aziis98.vrenderer.api;
 
+import java.awt.image.*;
+
 public class Canvas {
 
 
+    public BufferedImage renderImage(int width, int height) {
+        Config config = new Config();
+        config.width = width;
+        config.height = height;
+        return renderImage( config );
+    }
 
+    private BufferedImage renderImage(Config config) {
+        return null;
+    }
+
+    public static class Config {
+        private int width, height;
+        // ... render options ...
+    }
 }
